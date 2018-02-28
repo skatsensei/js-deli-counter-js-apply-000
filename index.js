658bdec77ef5e6d12b57dcc4637b8bf779b33937
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2018-02-28 15:37:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+let customer = katzDeliLine.shift()
+return `Currently serving ${customer}.`
+}
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+
+let resultString = "The line is currently:"
+
+for(let i = 0; i<katzDeliLine.length;i++){
+  resultString += ` ${i+1}. ${katzDeliLine[i]}`
+  if (i != katzDeliLine.length-1){
+  resultString +=","
+
+return resultString
+  }
+}
+
